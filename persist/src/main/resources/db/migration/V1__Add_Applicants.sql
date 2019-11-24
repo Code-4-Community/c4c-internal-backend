13719eab386fbbe1752cf3971e366903268ef4f3
Ryan Jung <ryan.matthew.jung@gmail.com>
2019-11-24 10:31:51 -0500

    schema update

---
+++ persist/src/main/resources/db/migration/V1__Add_Applicants.sql
@@ -0,0 +1,69 @@
+CREATE TABLE
+IF NOT EXISTS users
+(
+  id SERIAL
+PRIMARY KEY,
+  email VARCHAR 
+(255) UNIQUE,
+  first_name VARCHAR
+(255),
+  last_name VARCHAR
+(255),
+  hashed_password VARCHAR
+(255),
+  graduation_year INT,
+  major VARCHAR
+(255),
+  privilege_level INT
+);
+
+CREATE TABLE
+IF NOT EXISTS events
+(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR
+(255),
+  date TIMESTAMP,
+  open BOOLEAN,
+  code VARCHAR
+(255) UNIQUE
+);
+
+CREATE TABLE
+IF NOT EXISTS event_check_ins
+(
+  id SERIAL PRIMARY KEY,
+  user_id INT,
+  event_id INT,
+
+  CONSTRAINT fk_attended_user FOREIGN KEY
+(user_id) REFERENCES users
+(id),
+  CONSTRAINT fk_attended_event FOREIGN KEY
+(event_id) REFERENCES events
+(id)
+);
+
+CREATE TABLE
+IF NOT EXISTS applicants
+(
+  id SERIAL PRIMARY KEY,
+  user_id INT,
+  resume bytea,
+  file_type VARCHAR(15),
+  interests VARCHAR(255)[],
+  prior_involvement TEXT,
+  why_join TEXT,
+
+  CONSTRAINT fk_applicant_user FOREIGN KEY
+(user_id) REFERENCES users
+(id)
+
+);
+
+CREATE TABLE
+IF NOT EXISTS blacklisted_tokens
+(
+  id VARCHAR(255) NOT NULL PRIMARY KEY,
+  time_milliseconds BIGINT
+);
