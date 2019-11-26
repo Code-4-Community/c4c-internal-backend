21b7963f0be5f840cca5393c4de1d24ffe98be9e
Ryan Jung <ryan.matthew.jung@gmail.com>
2019-11-26 13:30:06 -0500

    added newsreturn

---
+++ api/src/main/java/com/codeforcommunity/dto/NewsReturn.java
@@ -0,0 +1,53 @@
+package com.codeforcommunity.dto;
+
+import java.time.LocalDateTime;
+
+public class NewsReturn {
+  // doesnt make sense that we would ever set these values after construction.
+  // favor immutibility.
+  private final int id;
+  private final String title;
+  private final String description;
+  private final String author;
+  private final LocalDateTime date;
+  private final String content;
+
+  public NewsReturn(int id, String title, String description, String author, LocalDateTime date, String content) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.author = author;
+    this.date = date;
+    this.content = content;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public String getTitle() {
+    return this.title;
+  }
+
+  public String getDescription() {
+    return this.description;
+  }
+
+  public String getAuthor() {
+    return this.author;
+  }
+
+  public LocalDateTime getDate() {
+    return this.date;
+  }
+
+  public String getContent() {
+    return this.content;
+  }
+
+  @Override
+  public String toString() {
+    return this.id + " " + this.title + " " + this.description + " " + this.author + " " + this.date.toString() + " "
+        + this.content;
+  }
+}
